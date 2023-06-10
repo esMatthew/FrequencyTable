9bd8008380f067005c104d5fe4bc831d68605ee2
esMatthew <118026823+esMatthew@users.noreply.github.com>
2023-06-10 17:16:34 -0500

    Added the class mark for the intervals

--- SortingAlgorithm/main.cpp
+++ SortingAlgorithm/main.cpp
@@ -100,7 +100,7 @@ int main() {
 
     cout << "Enter the elements of the array (separated by a space): " << endl;
 
-    int x;
+    float x;
     for(int i = 0; i < n; i++) {
         cin >> x;
         v.push_back(x);
